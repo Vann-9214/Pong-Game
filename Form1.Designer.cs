704f0f003905d0befcbaba4b3391853252604ae7
23-5183-981 Ivan Clement Cañete <ivanclement.canete@cit.edu>
2025-02-06 07:27:35 +0800

    Added Menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,13 +37,29 @@
             Menu = new Button();
             PlayerScore = new Label();
             ComputerScore = new Label();
-            panel1 = new Panel();
-            panel2 = new Panel();
-            panel3 = new Panel();
-            panel4 = new Panel();
+            PanelUp = new Panel();
+            PanelDown = new Panel();
+            PanelLeft = new Panel();
+            PanelRight = new Panel();
+            MenuPanel = new Panel();
+            Exit = new Button();
+            View = new Button();
+            Settings = new Button();
+            Restart = new Button();
+            Resume = new Button();
+            label1 = new Label();
+            Countdown = new Label();
+            SettingsPanel = new Panel();
+            Back = new Button();
+            PaddleSize = new Button();
+            BallSpeed = new Button();
+            Difficulty = new Button();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)Player).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Computer).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Ball).BeginInit();
+            MenuPanel.SuspendLayout();
+            SettingsPanel.SuspendLayout();
             SuspendLayout();
             // 
             // Player
@@ -91,6 +107,7 @@
             Menu.Size = new Size(55, 45);
             Menu.TabIndex = 4;
             Menu.UseVisualStyleBackColor = true;
+            Menu.Click += Menu_Click;
             // 
             // PlayerScore
             // 
@@ -116,37 +133,197 @@
             ComputerScore.TabIndex = 5;
             ComputerScore.Text = "Computer Score: 0";
             // 
-            // panel1
+            // PanelUp
             // 
-            panel1.BackColor = SystemColors.ButtonShadow;
-            panel1.Location = new Point(12, 54);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1414, 15);
-            panel1.TabIndex = 6;
+            PanelUp.BackColor = SystemColors.ButtonShadow;
+            PanelUp.Location = new Point(12, 54);
+            PanelUp.Name = "PanelUp";
+            PanelUp.Size = new Size(1414, 15);
+            PanelUp.TabIndex = 6;
             // 
-            // panel2
+            // PanelDown
             // 
-            panel2.BackColor = SystemColors.ButtonShadow;
-            panel2.Location = new Point(12, 616);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(1414, 15);
-            panel2.TabIndex = 7;
+            PanelDown.BackColor = SystemColors.ButtonShadow;
+            PanelDown.Location = new Point(12, 616);
+            PanelDown.Name = "PanelDown";
+            PanelDown.Size = new Size(1414, 15);
+            PanelDown.TabIndex = 7;
             // 
-            // panel3
+            // PanelLeft
             // 
-            panel3.BackColor = SystemColors.ButtonShadow;
-            panel3.Location = new Point(12, 54);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(15, 577);
-            panel3.TabIndex = 7;
+            PanelLeft.BackColor = SystemColors.ButtonShadow;
+            PanelLeft.Location = new Point(12, 54);
+            PanelLeft.Name = "PanelLeft";
+            PanelLeft.Size = new Size(15, 577);
+            PanelLeft.TabIndex = 7;
             // 
-            // panel4
+            // PanelRight
             // 
-            panel4.BackColor = SystemColors.ButtonShadow;
-            panel4.Location = new Point(1411, 54);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(15, 577);
-            panel4.TabIndex = 8;
+            PanelRight.BackColor = SystemColors.ButtonShadow;
+            PanelRight.Location = new Point(1411, 54);
+            PanelRight.Name = "PanelRight";
+            PanelRight.Size = new Size(15, 577);
+            PanelRight.TabIndex = 8;
+            // 
+            // MenuPanel
+            // 
+            MenuPanel.BackColor = Color.Gainsboro;
+            MenuPanel.Controls.Add(Exit);
+            MenuPanel.Controls.Add(View);
+            MenuPanel.Controls.Add(Settings);
+            MenuPanel.Controls.Add(Restart);
+            MenuPanel.Controls.Add(Resume);
+            MenuPanel.Controls.Add(label1);
+            MenuPanel.Location = new Point(419, 6);
+            MenuPanel.Name = "MenuPanel";
+            MenuPanel.Size = new Size(600, 604);
+            MenuPanel.TabIndex = 9;
+            // 
+            // Exit
+            // 
+            Exit.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic);
+            Exit.Location = new Point(69, 506);
+            Exit.Name = "Exit";
+            Exit.Size = new Size(462, 94);
+            Exit.TabIndex = 5;
+            Exit.Text = "Exit";
+            Exit.UseVisualStyleBackColor = true;
+            Exit.Click += Exit_Click;
+            // 
+            // View
+            // 
+            View.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic);
+            View.Location = new Point(69, 406);
+            View.Name = "View";
+            View.Size = new Size(462, 94);
+            View.TabIndex = 4;
+            View.Text = "View Control";
+            View.UseVisualStyleBackColor = true;
+            View.Click += View_Click;
+            // 
+            // Settings
+            // 
+            Settings.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic);
+            Settings.Location = new Point(69, 306);
+            Settings.Name = "Settings";
+            Settings.Size = new Size(462, 94);
+            Settings.TabIndex = 3;
+            Settings.Text = "Settings";
+            Settings.UseVisualStyleBackColor = true;
+            Settings.Click += Settings_Click;
+            // 
+            // Restart
+            // 
+            Restart.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            Restart.Location = new Point(69, 206);
+            Restart.Name = "Restart";
+            Restart.Size = new Size(462, 94);
+            Restart.TabIndex = 2;
+            Restart.Text = "Restart";
+            Restart.UseVisualStyleBackColor = true;
+            Restart.Click += Restart_Click;
+            // 
+            // Resume
+            // 
+            Resume.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            Resume.Location = new Point(69, 104);
+            Resume.Name = "Resume";
+            Resume.Size = new Size(462, 94);
+            Resume.TabIndex = 1;
+            Resume.Text = "Resume";
+            Resume.UseVisualStyleBackColor = true;
+            Resume.Click += Resume_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Silver;
+            label1.Font = new Font("Courier New", 35F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(3, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(594, 84);
+            label1.TabIndex = 0;
+            label1.Text = "Game Paused";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Countdown
+            // 
+            Countdown.AutoSize = true;
+            Countdown.Font = new Font("Impact", 15F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Countdown.ForeColor = SystemColors.ControlLightLight;
+            Countdown.Location = new Point(1068, 20);
+            Countdown.Name = "Countdown";
+            Countdown.Size = new Size(0, 32);
+            Countdown.TabIndex = 10;
+            // 
+            // SettingsPanel
+            // 
+            SettingsPanel.BackColor = Color.Gainsboro;
+            SettingsPanel.Controls.Add(Back);
+            SettingsPanel.Controls.Add(PaddleSize);
+            SettingsPanel.Controls.Add(BallSpeed);
+            SettingsPanel.Controls.Add(Difficulty);
+            SettingsPanel.Controls.Add(label2);
+            SettingsPanel.Location = new Point(419, 6);
+            SettingsPanel.Name = "SettingsPanel";
+            SettingsPanel.Size = new Size(600, 604);
+            SettingsPanel.TabIndex = 10;
+            // 
+            // Back
+            // 
+            Back.BackColor = Color.Gainsboro;
+            Back.BackgroundImage = (Image)resources.GetObject("Back.BackgroundImage");
+            Back.BackgroundImageLayout = ImageLayout.Stretch;
+            Back.FlatAppearance.BorderColor = Color.FromArgb(0, 0, 0, 0);
+            Back.FlatAppearance.BorderSize = 0;
+            Back.FlatAppearance.MouseOverBackColor = Color.White;
+            Back.FlatStyle = FlatStyle.Flat;
+            Back.Location = new Point(524, 90);
+            Back.Name = "Back";
+            Back.Size = new Size(73, 59);
+            Back.TabIndex = 4;
+            Back.UseVisualStyleBackColor = false;
+            Back.Click += Back_Click;
+            // 
+            // PaddleSize
+            // 
+            PaddleSize.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic);
+            PaddleSize.Location = new Point(69, 431);
+            PaddleSize.Name = "PaddleSize";
+            PaddleSize.Size = new Size(462, 94);
+            PaddleSize.TabIndex = 3;
+            PaddleSize.Text = "Paddle Size";
+            PaddleSize.UseVisualStyleBackColor = true;
+            // 
+            // BallSpeed
+            // 
+            BallSpeed.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            BallSpeed.Location = new Point(69, 294);
+            BallSpeed.Name = "BallSpeed";
+            BallSpeed.Size = new Size(462, 94);
+            BallSpeed.TabIndex = 2;
+            BallSpeed.Text = "Ball Speed";
+            BallSpeed.UseVisualStyleBackColor = true;
+            // 
+            // Difficulty
+            // 
+            Difficulty.Font = new Font("Lucida Console", 15F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            Difficulty.Location = new Point(69, 157);
+            Difficulty.Name = "Difficulty";
+            Difficulty.Size = new Size(462, 94);
+            Difficulty.TabIndex = 1;
+            Difficulty.Text = "Difficulty";
+            Difficulty.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Silver;
+            label2.Font = new Font("Courier New", 35F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(3, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(594, 84);
+            label2.TabIndex = 0;
+            label2.Text = "Settings";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form
             // 
@@ -154,16 +331,19 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Black;
             ClientSize = new Size(1438, 643);
-            Controls.Add(panel4);
-            Controls.Add(panel3);
-            Controls.Add(panel2);
-            Controls.Add(panel1);
+            Controls.Add(SettingsPanel);
+            Controls.Add(MenuPanel);
+            Controls.Add(Player);
+            Controls.Add(PanelRight);
+            Controls.Add(PanelLeft);
+            Controls.Add(PanelDown);
+            Controls.Add(PanelUp);
             Controls.Add(ComputerScore);
             Controls.Add(Menu);
             Controls.Add(PlayerScore);
             Controls.Add(Computer);
-            Controls.Add(Player);
             Controls.Add(Ball);
+            Controls.Add(Countdown);
             DoubleBuffered = true;
             Name = "Form";
             StartPosition = FormStartPosition.CenterScreen;
@@ -173,6 +353,8 @@
             ((System.ComponentModel.ISupportInitialize)Player).EndInit();
             ((System.ComponentModel.ISupportInitialize)Computer).EndInit();
             ((System.ComponentModel.ISupportInitialize)Ball).EndInit();
+            MenuPanel.ResumeLayout(false);
+            SettingsPanel.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -186,9 +368,23 @@
         private Button Menu;
         private Label PlayerScore;
         private Label ComputerScore;
-        private Panel panel1;
-        private Panel panel2;
-        private Panel panel3;
-        private Panel panel4;
+        private Panel PanelUp;
+        private Panel PanelDown;
+        private Panel PanelLeft;
+        private Panel PanelRight;
+        private Panel MenuPanel;
+        private Label label1;
+        private Button Resume;
+        private Button View;
+        private Button Settings;
+        private Button Restart;
+        private Button Exit;
+        private Label Countdown;
+        private Panel SettingsPanel;
+        private Button PaddleSize;
+        private Button BallSpeed;
+        private Button Difficulty;
+        private Label label2;
+        private Button Back;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,93 +1,110 @@
-
 namespace Pong_Game
-{ 
-        using System.Windows.Forms;
+{
+    using System.Drawing;
+    using System.Windows.Forms;
 
     public partial class Form : System.Windows.Forms.Form
     {
-        Timer pauseTimer = new Timer();
-
-        int ballxspeed = 7;
-        int ballyspeed = 7;
-        int speed = 5;
+        Timer ResumeTimer = new Timer();
+        Timer PauseTimer = new Timer();
+        int countdownresume = 3;
+        int ballxspeed = 8;
+        int ballyspeed = 8;
+        int speed = 6; // initial speed of the computer paddle
         Random rand = new Random();
         bool goDown, goUp;
         int computerSpeedChange = 50; // Cooldown Tick
         int playerScore = 0;
         int computerScore = 0;
         int playerSpeed = 7; // Speed of the Player Paddle
-        int[] i = { 5, 8, 10, 11}; // Speed of the Computer Paddle
-        int[] j = {10, 13, 14, 11, 12, 15 }; // Speed of the Ball, Both X and Y
+        int[] i = { 6, 8, 10, 11 }; // Speed of the Computer Paddle
+        int[] j = { 11, 14, 15, 12, 13, 16 }; // Speed of the Ball, Both X and Y
 
         public Form()
         {
             InitializeComponent();
-
-            pauseTimer.Interval = 2000;
-            pauseTimer.Tick += PauseTimer_Tick;
-
+            MenuPanel.Visible = false;
+            SettingsPanel.Visible = false;
+            PauseTimer.Interval = 2000; // Seconds of interval
+            ResumeTimer.Interval = 1000;
+            PauseTimer.Tick += PauseTimer_Tick;
+            ResumeTimer.Tick += ResumeTimer_Tick;
             this.KeyPreview = true;
 
+            Countdown.Location = new Point(((this.ClientSize.Width - Countdown.Width) / 2) - 90, (this.ClientSize.Height - Countdown.Height) / 2);
             Menu.Location = new Point((this.ClientSize.Width - Ball.Width) / 2, 6);
-            Player.Location = new Point(33, (this.ClientSize.Height - Player.Height) / 2);
-            Computer.Location = new Point(1375, (this.ClientSize.Height - Computer.Height) / 2);
+            Player.Location = new Point((PanelLeft.Left + 19), (this.ClientSize.Height - Player.Height) / 2);
+            Computer.Location = new Point((PanelRight.Left - 27), (this.ClientSize.Height - Computer.Height) / 2);
             Ball.Location = new Point((this.ClientSize.Width - Ball.Width) / 2, (this.ClientSize.Height - Ball.Height) / 2);
         }
-
         private void PauseTimer_Tick(object sender, EventArgs e)
         {
-            pauseTimer.Stop();
+            PauseTimer.Stop();
             GameTime.Start();
+        }
+        private void ResumeTimer_Tick(object sender, EventArgs e)
+        {
+            if (countdownresume > 1)
+            {
+                countdownresume--;
+                Countdown.Text = $"Game Resumes in {countdownresume}";
+
+            }
+            else
+            {
+                Countdown.Text = "";
+                ResumeTimer.Stop();
+                GameTime.Start();
+            }
         }
         private void GameTimerEvent(object sender, EventArgs e)
         {
             Ball.Top += ballyspeed;
             Ball.Left += ballxspeed;
 
-            PlayerScore.Text = $"Player Score: {playerScore}";
-            ComputerScore.Text = $"Computer Score: {computerScore}";
-
-            if (Ball.Top < 71 || Ball.Bottom > 616) // Ball Bounce in Boundaries
+            if (Ball.Top < PanelUp.Bottom || Ball.Bottom > PanelDown.Top) // Ball Bounce in Boundaries
             {
                 ballyspeed = -ballyspeed;
             }
             if (Ball.Left < -5) // When Computer Scores
             {
-                Player.Location = new Point(33, (this.ClientSize.Height - Player.Height) / 2);
-                Computer.Location = new Point(1375, (this.ClientSize.Height - Computer.Height) / 2);
+                Player.Location = new Point((PanelLeft.Left + 19), (this.ClientSize.Height - Player.Height) / 2);
+                Computer.Location = new Point((PanelRight.Left - 27), (this.ClientSize.Height - Computer.Height) / 2);
                 Ball.Left = this.ClientSize.Width / 2;
                 Ball.Top = (this.ClientSize.Height - 65) / 2;
                 ballxspeed = -7;
                 ballyspeed = -7;
                 computerScore++;
+                ComputerScore.Text = $"Computer Score: {computerScore}";
                 if (computerScore < 5)
                 {
                     GameTime.Stop();
-                    pauseTimer.Start();
+                    PauseTimer.Start();
                 }
             }
             if (Ball.Right > this.ClientSize.Width + 5) // When Player Scores
             {
-                Player.Location = new Point(33, (this.ClientSize.Height - Player.Height) / 2);
-                Computer.Location = new Point(1375, (this.ClientSize.Height - Computer.Height) / 2);
+                Player.Location = new Point((PanelLeft.Left + 19), (this.ClientSize.Height - Player.Height) / 2);
+                Computer.Location = new Point((PanelRight.Left - 27), (this.ClientSize.Height - Computer.Height) / 2);
                 Ball.Left = this.ClientSize.Width / 2;
                 Ball.Top = (this.ClientSize.Height - 65) / 2;
                 ballxspeed = 7;
                 ballyspeed = -7;
                 playerScore++;
+                PlayerScore.Text = $"Player Score: {playerScore}";
                 if (playerScore < 5)
                 {
                     GameTime.Stop();
-                    pauseTimer.Start();
+                    PauseTimer.Start();
                 }
             }
-            if (Computer.Top <= 71) // Ensures the Computer Paddle doesn't go out of bounds when on top
+            if (Computer.Top <= PanelUp.Bottom) // Ensures the Computer Paddle doesn't go out of bounds when on top
             {
-                Computer.Top = 71;
+                Computer.Top = PanelUp.Bottom;
             }
-            else if (Computer.Bottom >= 616) // Ensures the Computer Paddle doesn't go out of bounds when on bellow
+            else if (Computer.Bottom >= PanelDown.Top) // Ensures the Computer Paddle doesn't go out of bounds when on bellow
             {
-                Computer.Top = 616 - Computer.Height;
+                Computer.Top = PanelDown.Top - Computer.Height;
             }
 
             if (Ball.Top < Computer.Top + (Computer.Height / 2) && Ball.Left > (this.ClientSize.Width - Ball.Width) / 2) // Computer Movement Up
@@ -106,12 +123,12 @@ namespace Pong_Game
                 computerSpeedChange = 50;
             }
 
-            if (goDown && Player.Top + Player.Height < 616) // Player Movement Down
+            if (goDown && Player.Top + Player.Height < PanelDown.Top - 6) // Player Movement Down
             {
-               Player.Top += playerSpeed;   
+                Player.Top += playerSpeed;
             }
 
-            if (goUp && Player.Top > 71) // Player Movement Up
+            if (goUp && Player.Top > PanelUp.Bottom + 3) // Player Movement Up
             {
                 Player.Top -= playerSpeed;
             }
@@ -119,16 +136,15 @@ namespace Pong_Game
             CheckCollision(Ball, Player, Player.Right + 5);
             CheckCollision(Ball, Computer, Computer.Left - 35);
 
-            if (computerScore > 5)
+            if (computerScore == 5)
             {
                 GameOver("You Lost the game bruh.");
             }
-            else if (playerScore > 5)
+            else if (playerScore == 5)
             {
                 GameOver("You won the game bruh.");
             }
         }
-
         private void KeyIsDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
@@ -140,7 +156,6 @@ namespace Pong_Game
                 goDown = true;
             }
         }
-
         private void KeyIsUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
@@ -152,7 +167,6 @@ namespace Pong_Game
                 goDown = false;
             }
         }
-
         private void CheckCollision(PictureBox PicOne, PictureBox PicTwo, int offset)
         {
             if (PicOne.Bounds.IntersectsWith(PicTwo.Bounds))
@@ -181,16 +195,66 @@ namespace Pong_Game
                 }
             }
         }
-
         private void GameOver(string Message)
         {
             GameTime.Stop();
             MessageBox.Show(Message, "Moo Says: ");
             computerScore = 0;
             playerScore = 0;
-            ballxspeed = ballyspeed = 4;
+            ballxspeed = ballyspeed = 8;
             computerSpeedChange = 50;
             GameTime.Start();
+        }
+        private void Menu_Click(object sender, EventArgs e)
+        {
+            Countdown.Visible = false;
+            MenuPanel.Visible = true;
+            GameTime.Stop();
+            ResumeTimer.Stop();
+            PauseTimer.Stop();
+        }
+        private void Resume_Click(object sender, EventArgs e)
+        {          
+            countdownresume = 3;
+            Countdown.Visible = true;
+            Countdown.Text = $"Game Resumes in {countdownresume}";
+            ResumeTimer.Start();
+            MenuPanel.Visible = false;
+        }
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            PauseTimer.Stop();
+            ResumeTimer.Stop();
+            computerScore = 0;
+            playerScore = 0;
+            PlayerScore.Text = $"Player Score: {playerScore}";
+            ComputerScore.Text = $"Computer Score: {computerScore}";
+            Player.Location = new Point((PanelLeft.Left + 19), (this.ClientSize.Height - Player.Height) / 2);
+            Computer.Location = new Point((PanelRight.Left - 27), (this.ClientSize.Height - Computer.Height) / 2);
+            Ball.Location = new Point((this.ClientSize.Width - Ball.Width) / 2, (this.ClientSize.Height - Ball.Height) / 2);
+            ballxspeed = ballyspeed = 8;
+            computerSpeedChange = 50;
+            PauseTimer.Start();
+            MenuPanel.Visible = false;
+        }
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            SettingsPanel.Visible = true;
+            MenuPanel.Visible = false;
+        }
+        private void View_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            SettingsPanel.Visible = false;
+            MenuPanel.Visible = true;
         }
     }
 }
